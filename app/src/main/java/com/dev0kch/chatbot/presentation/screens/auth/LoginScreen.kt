9173eccaaf2a4f2d7f0739c90406920d3269e377
mobelite-fkchaou <fkchaou@mobelite.fr>
2024-06-25 17:36:09 +0100

    fixbug(Login Screen): - Add Loading. #fixbug /fix_login_process

--- app/src/main/java/com/dev0kch/chatbot/presentation/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/dev0kch/chatbot/presentation/screens/auth/LoginScreen.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,8 +22,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -42,7 +44,9 @@ import com.dev0kch.chatbot.ui.theme.textColorHint
 import com.dev0kch.chatbot.ui.theme.white
 import com.dev0kch.chatbot.utils.GlobalStyles
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.dev0kch.chatbot.ui.theme.textError
 import com.dev0kch.chatbot.utils.Resource
+import com.dev0kch.chatbot.utils.STRINGS
 import com.dev0kch.chatbot.utils.validateEmail
 
 
@@ -53,7 +57,13 @@ fun LoginScreen(
     authenticationViewModel: AuthenticationViewModel = hiltViewModel()
 ) {
 
-    val scope = rememberCoroutineScope()
+
+    var isEmailError by remember { mutableStateOf("") }
+    var isPasswordError by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var loading by remember { mutableStateOf(false) }
+
 
     val loginFlow by authenticationViewModel.loginFLow.collectAsState()
 
@@ -63,140 +73,234 @@ fun LoginScreen(
                 // Handle failure
                 Log.println(Log.ASSERT, "Login", "Failure")
             }
+
             is Resource.Loading -> {
                 // Show loading
                 Log.println(Log.ASSERT, "Login", "Loading")
+                loading = true
             }
+
             is Resource.Success -> {
-                // Handle success
+                //        Handle success
                 Log.println(Log.ASSERT, "Login", "Success")
                 navController?.navigate(Route.HomeScreen.route)
             }
+
             else -> {}
         }
     }
+
+    fun login() {
+        var isValidCredential = true
+
+        if (email.isEmpty() || password.isEmpty()) {
+            isEmailError = if (email.isEmpty()) {
+                STRINGS.EMAIL_IS_REQUIRED
+
+            } else {
+                ""
+            }
+            if (password.isEmpty()) {
+                isPasswordError = STRINGS.PASSWORD_IS_REQUIRED
+
+            } else {
+                isPasswordError
+            }
+            return;
+        } else {
+            if (!validateEmail(email)) {
+                isEmailError = STRINGS.ENTER_VALID_EMAIL
+                isValidCredential = false
+            } else {
+                ""
+            }
+            Log.println(Log.ASSERT, "Password Length", password.length.toString())
+
+            if (password.length < 6) {
+                isPasswordError = STRINGS.PASSWORD_MUST_BE_LEAST_6CH
+                isValidCredential = false
+
+            } else {
+                ""
+            }
+            if (isValidCredential) {
+                isPasswordError = ""
+                isEmailError = ""
+                authenticationViewModel.login(email, password)
+
+            }
+
+        }
+
+
+    }
+
     val listColors = listOf(second, primary)
 
 
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    if (loading) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(background)
-            .padding(top = GlobalStyles.Padding.ScreenPadding)
-    ) {
-
-        Text(
-            text = stringResource(id = R.string.txt_login_into_account),
-            modifier = Modifier.padding(
-                top = GlobalStyles.Padding.ScreenPadding,
-                start = GlobalStyles.Padding.ScreenPadding,
-                end = GlobalStyles.Padding.ScreenPadding,
-                bottom = 10.dp
-
-
-            ),
-            color = white,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.W600
-        )
-
-        Text(
-            text = stringResource(id = R.string.txt_wlcm_back),
-            modifier = Modifier.padding(
-                start =
-                GlobalStyles.Padding.ScreenPadding,
-            ),
-            color = textColorHint,
-            fontSize = 13.sp,
-            fontWeight = FontWeight.W400
-        )
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(background),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.security),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(200.dp)
-                    .padding(top = 50.dp),
-                contentScale = ContentScale.Crop,
-
-                )
-        }
-
-        TextField(
-            shape = RoundedCornerShape(5),
-            value = email,
-            onValueChange = { email = it },
-            label = { Text(text = stringResource(id = R.string.txt_email), fontSize = 13.sp) },
-            modifier = Modifier
-
-                .fillMaxWidth()
-                .padding(
-                    GlobalStyles.Padding.ScreenPadding,
-                ),
-        )
-
-        TextField(
-            shape = RoundedCornerShape(5),
-
-            value = password,
-            onValueChange = { password = it },
-            label = { Text(text = stringResource(id = R.string.txt_password), fontSize = 13.sp) },
-            modifier = Modifier
-
-                .fillMaxWidth()
-                .padding(
-                    start = GlobalStyles.Padding.ScreenPadding,
-                    end = GlobalStyles.Padding.ScreenPadding
-                )
-        )
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    top = 10.dp,
-                    end = GlobalStyles.Padding.ScreenPadding
-                ),
-            horizontalArrangement = Arrangement.End
-        ) {
-            Text(
-                text = stringResource(id = R.string.txt_forget_password),
-                color = textColorHint,
-                fontSize = 13.sp, fontWeight = FontWeight.W700
+            CircularProgressIndicator(
+                modifier = Modifier.width(64.dp),
+                color = primary,
             )
         }
 
-        GradientButton(
-            gradientColors = listColors,
-            cornerRadius = 5.dp, nameButton = stringResource(id = R.string.txt_login),
-            roundedCornerShape = RoundedCornerShape(5.dp),
-            modifier = Modifier.padding(top = 50.dp),
-            onClick = { login(email, password, authenticationViewModel, navController) }
-        )
-    }
+    } else
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(background)
+                .padding(top = GlobalStyles.Padding.ScreenPadding)
+        ) {
+
+
+            Text(
+                text = stringResource(id = R.string.txt_login_into_account),
+                modifier = Modifier.padding(
+                    top = GlobalStyles.Padding.ScreenPadding,
+                    start = GlobalStyles.Padding.ScreenPadding,
+                    end = GlobalStyles.Padding.ScreenPadding,
+                    bottom = 10.dp
+
+
+                ),
+                color = white,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.W600
+            )
+
+            Text(
+                text = stringResource(id = R.string.txt_wlcm_back),
+                modifier = Modifier.padding(
+                    start =
+                    GlobalStyles.Padding.ScreenPadding,
+                ),
+                color = textColorHint,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.W400
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.security),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(200.dp)
+                        .padding(top = 50.dp),
+                    contentScale = ContentScale.Crop,
+
+                    )
+            }
+
+            TextField(
+                shape = RoundedCornerShape(5),
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(text = stringResource(id = R.string.txt_email), fontSize = 13.sp) },
+                modifier = Modifier
+
+                    .fillMaxWidth()
+                    .padding(
+                        top = GlobalStyles.Padding.ScreenPadding,
+                        start = GlobalStyles.Padding.ScreenPadding,
+                        end = GlobalStyles.Padding.ScreenPadding
+                    ),
+            )
+
+            Text(
+                text = isEmailError,
+                color = textError,
+                fontSize = GlobalStyles.Text.H5,
+                modifier = Modifier
+
+                    .fillMaxWidth()
+                    .padding(
+                        top = 2.dp,
+                        bottom = 5.dp,
+                        start = GlobalStyles.Padding.ScreenPadding,
+                        end = GlobalStyles.Padding.ScreenPadding
+
+                    ),
+            )
+
+
+
+            TextField(
+                shape = RoundedCornerShape(5),
+
+                value = password,
+                onValueChange = { password = it },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.txt_password),
+                        fontSize = 13.sp
+                    )
+                },
+                modifier = Modifier
+
+                    .fillMaxWidth()
+                    .padding(
+                        start = GlobalStyles.Padding.ScreenPadding,
+                        end = GlobalStyles.Padding.ScreenPadding
+                    ),
+
+
+                )
+
+            Text(
+                text = isPasswordError,
+                color = textError,
+                fontSize = GlobalStyles.Text.H5,
+                modifier = Modifier
+
+                    .fillMaxWidth()
+                    .padding(
+                        top = 2.dp,
+                        start = GlobalStyles.Padding.ScreenPadding,
+                        end = GlobalStyles.Padding.ScreenPadding
+                    ),
+            )
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+
+                        end = GlobalStyles.Padding.ScreenPadding
+                    ),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Text(
+                    text = stringResource(id = R.string.txt_forget_password),
+                    color = textColorHint,
+                    fontSize = 13.sp, fontWeight = FontWeight.W700
+                )
+            }
+
+            GradientButton(
+                gradientColors = listColors,
+                cornerRadius = 5.dp, nameButton = stringResource(id = R.string.txt_login),
+                roundedCornerShape = RoundedCornerShape(5.dp),
+                modifier = Modifier.padding(top = 50.dp),
+                onClick = { login() }
+            )
+        }
+
 }
 
-private fun login(
-
-    email: String,
-    password: String,
-    authenticationViewModel: AuthenticationViewModel, navController: NavHostController?
-) {
-
-    if (validateEmail(email) && password.length > 6) {
-
-            authenticationViewModel.login(email, password)
-
-    }
-
-}
 
 @Preview
 @Composable
